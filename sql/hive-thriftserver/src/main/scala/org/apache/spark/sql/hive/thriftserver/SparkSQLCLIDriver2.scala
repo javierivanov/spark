9ccfc9eda53b493d39fcf59e0d814cf520061d5c
Javier Fuentes <j.fuentes.m@icloud.com>
2019-12-05 12:11:04 -0300

    added new spark sql client

---
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver2.scala
@@ -0,0 +1,17 @@
+package org.apache.spark.sql.hive.thriftserver
+
+import org.apache.hadoop.hive.cli.OptionsProcessor
+import org.apache.spark.internal.Logging
+
+
+private[hive] class SparkSQLCLIDriver2 extends Logging {
+
+}
+
+
+private[hive] object SparkSQLCLIDriver2 extends Logging {
+  def main(args: Array[String]): Unit = {
+
+
+  }
+}
