acc65fbba73b8010558224ac89ac5ba32609e230
Javier Fuentes <j.fuentes.m@icloud.com>
2019-12-08 23:12:33 -0300

    added new args parser

--- sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver2.scala
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver2.scala
@@ -1,17 +1,27 @@
 package org.apache.spark.sql.hive.thriftserver
 
-import org.apache.hadoop.hive.cli.OptionsProcessor
+
+import org.apache.spark.SparkConf
+import org.apache.spark.deploy.SparkHadoopUtil
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.hive.HiveUtils
 
 
-private[hive] class SparkSQLCLIDriver2 extends Logging {
-
-}
 
 
 private[hive] object SparkSQLCLIDriver2 extends Logging {
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
+        val sparkSQLArgs = SparkSQLCLIArguments(args)
+        sparkSQLArgs.parse()
 
+        val sparkConf = new SparkConf(loadDefaults = true)
+        sparkConf.setAll(sparkSQLArgs.getSparkConfs)
+        val hadoopConf = SparkHadoopUtil.get.newConfiguration(sparkConf)
+        val extraConfigs = HiveUtils.formatTimeVarsForHiveClient(hadoopConf)
 
-  }
+    }
+}
+
+private[hive] class SparkSQLCLIDriver2 extends Logging {
+
 }
